fbc801e8f9505c40bcd4e94e52d1ba98c963f714
ooo <ooo@ooo.ooo>
2020-10-16 14:31:28 +0200

    Chunk changes: - NEP #1 naming convention - Changed enum to pure

--- nim/meta/chunk.nim
+++ nim/meta/chunk.nim
@@ -1,58 +1,64 @@
+## The module dedicated to the type Chunk.
+## A chunk is a piece of bytecode.
+
 import valueobject
 
 
 type
-    OpCode* = enum
-        OP_CONSTANT = 0u8,
-        OP_CONSTANT_LONG,
-        OP_RETURN,
-        OP_NEGATE,
-        OP_ADD,
-        OP_SUBTRACT,
-        OP_DIVIDE,
-        OP_MULTIPLY,
-        OP_POW,
-        OP_MOD,
-        OP_NIL,
-        OP_TRUE,
-        OP_FALSE,
-        OP_GREATER,
-        OP_LESS,
-        OP_EQUAL,
-        OP_NOT,
-        OP_SLICE,
-        OP_SLICE_RANGE,
-        OP_POP,
-        OP_DEFINE_GLOBAL,
-        OP_GET_GLOBAL,
-        OP_SET_GLOBAL,
-        OP_DELETE_GLOBAL,
-        OP_SET_LOCAL,
-        OP_GET_LOCAL,
-        OP_DELETE_LOCAL,
-        OP_JUMP_IF_FALSE,
-        OP_JUMP,
-        OP_LOOP,
-        OP_BREAK,
-        OP_SHR,
-        OP_SHL,
-        OP_NAN,
-        OP_INF,
-        OP_XOR,
-        OP_CALL,
-        OP_BOR,
-        OP_BAND,
-        OP_BNOT
+    OpCode* {.pure.} = enum
+      ## Enum of possible opcodes.
+      Constant = 0u8,
+      ConstantLong,
+      Return,
+      Negate,
+      Add,
+      Subtract,
+      Divide,
+      Multiply,
+      Pow,
+      Mod,
+      Nil,
+      True,
+      False,
+      Greater,
+      Less,
+      Equal,
+      Not,
+      Slice,
+      SliceRange,
+      Pop,
+      DefineGlobal,
+      GetGlobal,
+      SetGlobal,
+      DeleteGlobal,
+      SetLocal,
+      GetLocal,
+      DeleteLocal,
+      JumpIfFalse,
+      Jump,
+      Loop,
+      Breal,
+      Shr,
+      Shl,
+      Nan,
+      Inf,
+      Xor,
+      Call,
+      Bor,
+      Band,
+      Bnot
 
 
     Chunk* = ref object
-        consts*: ValueArray
-        code*: seq[uint8]
-        lines*: seq[int]
+      ## A piece of bytecode.
+      consts*: ValueArray
+      code*: seq[uint8]
+      lines*: seq[int]
 
 
-proc initChunk*(): Chunk =
-    result = Chunk(consts: ValueArray(values: @[]), code: @[], lines: @[])
+proc newChunk*(): Chunk =
+  ## 
+  result = Chunk(consts: ValueArray(values: @[]), code: @[], lines: @[])
 
 
 proc writeChunk*(self: Chunk, byt: uint8, line: int) =
